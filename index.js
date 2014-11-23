887606c485d7b7227606602995340fa936539bb7
Montana Flynn <montana@montanaflynn.me>
2014-11-22 23:13:15 -0800

    Use nock for testing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var unirest = require('unirest')
+var statusCodes = require('./status-codes.js')
 
 module.exports = function getStatus(url, cb) {
 
@@ -31,40 +32,5 @@ module.exports = function getStatus(url, cb) {
 }
 
 function getResponseMessage(status) {
-  var ResponseTypes = {
-    400: "Bad Request",
-    401: "Unauthorized",
-    402: "Payment Required",
-    403: "Forbidden",
-    404: "Not Found",
-    405: "Method Not Allowed",
-    406: "Not Acceptable",
-    407: "Proxy Authentication Required",
-    408: "Request Timeout",
-    409: "Conflict",
-    411: "Length Required",
-    412: "Precondition Failed",
-    413: "Payload Too Large",
-    414: "URI Too Long",
-    415: "Unsupported Media Type",
-    416: "Range Not Satisfied",
-    417: "Expectation Failed",
-    418: "Im A Teapot",
-    423: "Locked",
-    428: "Precondition Required",
-    429: "Too Many Requests",
-    500: "Internal Server Error",
-    501: "Not Implemented",
-    502: "Bad Gateway",
-    503: "Service Unavailable",
-    504: "Gateway Timeout",
-    505: "HTTP Version Not Supported",
-    507: "Insufficient Storage",
-    508: "Loop Detected",
-    510: "Not Extended",
-    511: "Network Authentication Required"
-  }
-
-  return ResponseTypes[status]
-
+  return statusCodes[status] || "Status"
 }
--- test.js
+++ test.js
@@ -1,15 +1,19 @@
 var urlStatus = require('./index.js')
 var assert = require('assert')
+var nock = require('nock');
+
+nock('http://localhost:9876').get('/ok').reply(200,"").persist();
+nock('http://localhost:9876').get('/404').reply(404,"").persist();
 
 describe('Online', function () {
   it('should return message "Online"', function(done){
-    urlStatus('http://httpbin.org/get', function(status){
+    urlStatus('http://localhost:9876/ok', function(status){
       assert.equal(status.message, "Online")
       done()
     })
   })
   it('should return status "200"', function(done){
-    urlStatus('http://httpbin.org/get', function(status){
+    urlStatus('http://localhost:9876/ok', function(status){
       assert.equal(status.status, 200)
       done()
     })
@@ -18,29 +22,35 @@ describe('Online', function () {
 
 describe('Offline', function () {
   it('should return message "Offline"', function(done){
-    urlStatus('http://httpbindfsdf.org/get', function(status){
+    urlStatus('http://nodomain:9876/', function(status){
       assert.equal(status.message, "Offline")
       done()
     })
   })
   it('should return status "false"', function(done){
-    urlStatus('http://httpbindfsdf.org/get', function(status){
+    urlStatus('http://nodomain:9876/', function(status){
       assert.equal(status.status, false)
       done()
     })
   })
 })
 
-describe('Errors', function () {
+describe('Codes', function () {
   it('should return message "Not Found"', function(done){
-    urlStatus('http://httpbin.org/status/404', function(status){
+    urlStatus('http://localhost:9876/404', function(status){
       assert.equal(status.message, "Not Found")
       done()
     })
   })
   it('should return status "404"', function(done){
-    urlStatus('http://httpbin.org/status/404', function(status){
+    urlStatus('http://localhost:9876/404', function(status){
       assert.equal(status.status, "404")
+      done()
+    })
+  })
+  it('should return message "Status"', function(done){
+    urlStatus('http://www.mocky.io/v2/54717a25db32047d00fed9fe', function(status){
+      assert.equal(status.message, "Status")
       done()
     })
   })
